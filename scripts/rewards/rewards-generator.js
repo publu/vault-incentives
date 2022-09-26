92dc35bb589a96ad6891fd5e61e59b36e6789dbe
Pablo <pablo@hashingsystems.com>
2022-09-26 18:28:07 -0500

    miss

--- scripts/rewards/rewards-generator.js
+++ scripts/rewards/rewards-generator.js
@@ -403,7 +403,7 @@ let vIncentives;
   } else{
 
     let isWeek1 = ((Number(vaultIncentivesFile)) % 2);
-    let vaultIncentivesFile = (isWeek1) ? vaultIncentivesFile : (Number(vaultIncentivesFile) - 1)
+    vaultIncentivesFile = (isWeek1) ? vaultIncentivesFile : (Number(vaultIncentivesFile) - 1)
     incentiveData = JSON.parse(fs.readFileSync("configs/week" + (vaultIncentivesFile) + ".json"))
     currentCalculation = vaultIncentivesFile;
   }
